11e39333a7dd636887e2b25a8734a0e4fc79bd07
Piotr Bednarek <piotrek.tt@gmail.com>
2015-08-13 01:22:01 +0200

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e 
 
+#this will make it work with jenkins when you setup virtualenv builder
 #. ~/.virtualenvs/python2.7/bin/activate
 
 PYTHONPATH=. python -m pystache.commands.test
